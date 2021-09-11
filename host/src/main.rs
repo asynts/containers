3a588311365c0fbd0fe9d7321d66a7d6ea7908ac
Paul Scharnofske <asynts@gmail.com>
2021-09-11 17:30:40 +0200

    Implement everything
    Everything appears to work, but I should rewrite it in C or at least
    C++.

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,5 +1,7 @@
 // Launch system in isolated environment.
 
+// FIXME: Maybe rewrite this in C?
+
 extern crate tempfile;
 extern crate libc;
 
@@ -29,30 +31,62 @@ fn prepare_container_directory() {
         "/home/me/dev/containers/target/x86_64-unknown-linux-musl/debug/asynts-containers-system",
         std::path::Path::new(CONTAINER_DIRECTORY).join("app")
     ).unwrap();
+
+    // This will be used by 'pivot_root'.
+    std::fs::create_dir(std::path::Path::new(CONTAINER_DIRECTORY).join("root")).unwrap();
 }
 
 extern "C"
 fn child_main(_: *mut libc::c_void) -> libc::c_int {
-    // FIXME: Set mount type MS_SLAVE
+    // Set propapation type of all mounts.  Since we ran 'clone' with 'CLONE_NEWNS', this will
+    // not affect anyone else.
+    unsafe {
+        let root_cstring = std::ffi::CString::new("/").unwrap();
+        let retval = libc::mount(std::ptr::null(), root_cstring.as_ptr(), std::ptr::null(), libc::MS_REC | libc::MS_PRIVATE, std::ptr::null());
+        assert_eq!(retval, 0);
+    }
 
-    // FIXME: Mount CONTAINER_DIRECTORY?
+    // To be able to utilize 'pivot_root', the target directory needs to be a mount point.
+    unsafe {
+        let source_cstring = std::ffi::CString::new(CONTAINER_DIRECTORY).unwrap();
+        let retval = libc::mount(source_cstring.as_ptr(), source_cstring.as_ptr(), std::ptr::null(), libc::MS_BIND, std::ptr::null());
+        assert_eq!(retval, 0);
+    }
 
-    // FIXME: Call pivot_root
+    // Now we swap the mount points, the old root becomes '/tmp/asynts-containers/root' the new
+    // root becomes '/tmp/asynts-containers'.
+    unsafe {
+        let newroot_cstring = std::ffi::CString::new(CONTAINER_DIRECTORY).unwrap();
+        let oldroot_cstring = std::ffi::CString::new("/tmp/asynts-containers/root").unwrap();
+        let retval = libc::syscall(libc::SYS_pivot_root, newroot_cstring.as_ptr(), oldroot_cstring.as_ptr());
+        assert_eq!(retval, 0);
+    }
 
-    std::os::unix::fs::chroot(CONTAINER_DIRECTORY).unwrap();
+    // FIXME: I think this is implied by 'pivot_root'?
     std::env::set_current_dir("/").unwrap();
 
+    // Now unmount the old root to make it disappear.
+    unsafe {
+        let target_cstring = std::ffi::CString::new("root").unwrap();
+        let retval = libc::umount2(target_cstring.as_ptr(), libc::MNT_DETACH);
+        assert_eq!(retval, 0);
+    }
+
+    std::fs::remove_dir("root").unwrap();
+
+    println!("Prepared everything, handing over to application!");
+
+    // We are now completely isolated from everything else.
     std::process::Command::new("./app")
         .env_clear()
         .exec();
     
     unreachable!();
 }
-fn create_container_process() {
+fn create_container_process() -> i32 {
     let stack_size = 0x1000;
     let mut stack: Vec<u8> = vec![0; stack_size];
 
-    // FIXME: Save process id
     unsafe {
         // FIXME: Do all the flags make sense?
         libc::clone(
@@ -60,7 +94,7 @@ fn create_container_process() {
             stack.as_mut_ptr().offset(stack_size.try_into().unwrap()) as *mut libc::c_void,
             libc::CLONE_NEWCGROUP | libc::CLONE_NEWIPC | libc::CLONE_NEWNET | libc::CLONE_NEWNS | libc::CLONE_NEWPID | libc::CLONE_NEWUSER | libc::CLONE_NEWUTS,
             std::ptr::null_mut()
-        );
+        )
     }
 }
 
@@ -69,7 +103,11 @@ fn main() {
 
     prepare_container_directory();
 
-    let _container_process = create_container_process();
+    let container_process_id = create_container_process();
 
-    // FIXME: Wait for process to finish.
+    // Wait for process to finish
+    unsafe {
+        while libc::waitpid(container_process_id, std::ptr::null_mut(), 0) != container_process_id {
+        }
+    }
 }
