9560f9acda226250cf159c9c4022fcaefc4c629e
Paul Scharnofske <asynts@gmail.com>
2021-09-11 14:49:23 +0200

    Make a more sophisticated plan

--- host/src/main.rs
+++ host/src/main.rs
@@ -4,6 +4,25 @@ extern crate tempfile;
 
 use std::os::unix::process::CommandExt;
 
+    // let working_directory = /* ... */;
+    // chmod(/* ... */);
+
+    // unshare(CLONE_NEWCGROUP | CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWTIME | CLONE_NEWUSER | CLONE_NEWUTS)
+    // // Not all 'unshare' operations can be applied directly, we need to create a new process
+    // // that does the required operations.
+    // fork()
+
+    // // If we don't do this, we could modify the mounts of the host.  This appears to be
+    // // necessary for 'pivot_root'.
+    // mount(NULL, working_directory, NULL, MS_SLAVE, NULL);
+
+    // // This should prevent a super user from escaping the chroot.
+    // chdir(working_directory)
+    // pivot_root(.)
+    // chroot(.)
+
+    // execve(/* ... */)
+
 fn main() {
     // FIXME: setup namespace
     // FIXME: all the other stuff
@@ -17,6 +36,8 @@ fn main() {
 
     std::os::unix::fs::chroot(working_directory.path()).unwrap();
     std::env::set_current_dir("/").unwrap();
+
+    // FIXME: We are still root here!
 
     std::process::Command::new("./app")
         .exec();
