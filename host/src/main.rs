abefc38e908d35a8863f32e37da8951fe1359e06
Paul Scharnofske <asynts@gmail.com>
2021-09-11 13:49:56 +0200

    Create temporary directory for 'chroot'.

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,15 +1,24 @@
 // Start target in isolated environment.
 
+extern crate tempfile;
+
 use std::os::unix::process::CommandExt;
 
 fn main() {
     // FIXME: setup namespace
     // FIXME: all the other stuff
 
-    std::os::unix::fs::chroot("/home/me/dev/containers/target/x86_64-unknown-linux-musl/debug/").unwrap();
+    let working_directory = tempfile::tempdir().unwrap();
+
+    std::fs::copy(
+        "/home/me/dev/containers/target/x86_64-unknown-linux-musl/debug/asynts-containers-system",
+        working_directory.path().join("app")
+    ).unwrap();
+
+    std::os::unix::fs::chroot(working_directory.path()).unwrap();
     std::env::set_current_dir("/").unwrap();
 
-    std::process::Command::new("./asynts-containers-system")
+    std::process::Command::new("./app")
         .exec();
     
     unreachable!();
--- system/src/main.rs
+++ system/src/main.rs
@@ -2,4 +2,9 @@
 
 fn main() {
     println!("I am executing in an isolated environment!");
+
+    println!("Here are all the files I have access to:");
+    for entry in walkdir::WalkDir::new("/") {
+        println!("  {}", entry.unwrap().path().display());
+    }
 }
