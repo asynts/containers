907a4fd502eaba0e4dec4d0b8ee619e496fc7b3a
Paul Scharnofske <asynts@gmail.com>
2021-09-18 11:41:31 +0200

    Verify that we can not escape by remount

--- asynts-jail-example/src/main.rs
+++ asynts-jail-example/src/main.rs
@@ -15,8 +15,6 @@ fn _test_filesystem_access() {
     }
 }
 
-// FIXME: Currently, we do not appear to have permissions to 'mount', if we had, would this
-//        be an issue?
 fn _test_remount_escape() {
     std::fs::create_dir("/foo").unwrap();
     nix::mount::mount::<str, str, str, str>(Some("/"), "/foo", None, nix::mount::MsFlags::MS_BIND, None).unwrap();
@@ -26,7 +24,7 @@ fn _test_unmount_root() {
     nix::mount::umount("/").unwrap();
 }
 
-fn test_bind_mount() {
+fn _test_bind_mount() {
     std::fs::create_dir("/foo").unwrap();
     std::fs::create_dir("/bar").unwrap();
 
@@ -37,6 +35,4 @@ fn test_bind_mount() {
 
 fn main() {
     assert!(util::is_statically_linked());
-
-    test_bind_mount();
 }
