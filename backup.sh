d9631247d57f467fbc26c2121630fba05ff02cc0
Paul Scharnofske <31994781+asynts@users.noreply.github.com>
2024-06-06 18:04:55 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,5 +3,4 @@ set -e
 
 ~/dev/scripts/backup.rb \
     --name "jail" \
-    --url "git@github.com:asynts/jail.git" \
-    --upload "s3://backup.asynts.com/git/jail"
+    --url "git@github.com:asynts/jail.git"
